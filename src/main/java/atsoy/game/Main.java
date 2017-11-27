d7756d1657e1e885230df01d02dbd08309dc345e
Lils2013 <aleksandr.tsoy@phystech.edu>
2017-11-27 23:42:30 +0300

    cool

---
+++ src/main/java/atsoy/game/Main.java
@@ -0,0 +1,19 @@
+package atsoy.game;
+
+import atsoy.engine.GameEngine;
+import atsoy.engine.IGameLogic;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        try {
+            boolean vSync = true;
+            IGameLogic gameLogic = new DummyGame();
+            GameEngine gameEng = new GameEngine("GAME", 600, 480, vSync, gameLogic);
+            gameEng.start();
+        } catch (Exception excp) {
+            excp.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
